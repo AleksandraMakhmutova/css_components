3b429c269abf467c916cd63a78ee37a3a94bda2d
AleksandraMakhmutova <honeyms@mailru>
2021-11-21 16:25:58 +0200

    add btn text

--- css-lessons/src/pages/mainPage/mainPage.tsx
+++ css-lessons/src/pages/mainPage/mainPage.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import classNames from "classnames/bind";
+import BtnText from "../../components/btn-text/btnText";
 
 import style from "./mainPage.module.scss";
 
 const cx = classNames.bind(style);
+
 export default function MainPage() {
   return (
     <section className={cx("section-about")}>
@@ -13,7 +14,23 @@ export default function MainPage() {
         </h2>
       </div>
       <div className={cx("row")}>
-        <div className={cx("col-1-of-2")}>Text</div>
+        <div className={cx("col-1-of-2")}>
+          <h3 className={cx("heading-tertiary")}>You're going to travel</h3>
+          <p className={cx("paragraph")}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            temporibus quis porro minima culpa fuga qui delectus consectetur
+            maiores modi, dolorum cum repudiandae deleniti illo tempore
+            veritatis hic itaque repellat?
+          </p>
+          <h3 className={cx("heading-tertiary")}>
+            Live adventures like you never have before
+          </h3>
+          <p className={cx("paragraph")}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            temporibus quis porro minima culpa fuga qui delectus consectetur.
+          </p>
+          <BtnText />
+        </div>
         <div className={cx("col-1-of-2")}>Image composition</div>
       </div>
     </section>
